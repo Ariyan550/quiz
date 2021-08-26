0cbb48a512c5056c8940d847ed8341500ff5b77c
Ariyan550 <mdpappuhassan560@gmail.com>
2021-08-26 07:20:32 -0700

    quiz

--- js/script.js
+++ js/script.js
@@ -1,16 +1,51 @@
 let startBtn =document.querySelector('.start');
 let exitBtn =document.querySelector('.exit');
+let conBtn =document.querySelector('.continue');
+let resBtn =document.querySelector('.restart');
+let quitBtn =document.querySelector('.quit');
+let nextBtn =document.querySelector('.nextBtn');
 let inBox =document.querySelector('.infoBox');
+let queBox =document.querySelector('.quizBox');
+let resultBox =document.querySelector('.result_box');
+
+
+
 
 startBtn.addEventListener('click', ()=>{
 inBox.classList.add('active');
 })
+
+
 
 exitBtn.addEventListener('click', ()=>{
 inBox.classList.remove('active');
 })
 
 
+conBtn.addEventListener('click', ()=>{
+queBox.classList.add('showQbox');
+
+})
+
+
+
+quitBtn.addEventListener('click', ()=>{
+resultBox.classList.remove('resHide');
+})
+
+
+nextBtn.addEventListener('click', ()=>{
+resultBox.classList.add('resHide');
+queBox.classList.remove('showQbox');
+inBox.classList.remove('active');
+
+})
+
+resBtn.addEventListener('click', ()=>{
+queBox.classList.add('showQbox');
+resultBox.classList.remove('resHide');
+
+})
 
 
 
